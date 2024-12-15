e8e6592938ab726d25a140aff4b7c4e8ec45afde
Gr44n91 <gr44n91@gmail.com>
2024-12-15 11:14:56 +0100

    actu de redaccion

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const challenges = [
     { question: "Reto 1: Ponme de lado y soy todo. Córtame por la mitad y no soy nada. ¿Qué soy?", answer: "8" },
     { question: "Reto 2: ¿Cuantos cuchillos hay en el cajón de los cubiertos de la cocina?", answer: "10" },
     { question: "Reto 3: ¿Cuantas Bolas tiene el arbol de navidad?", answer: "10" },
-    { question: "Reto 4: Tienes que llamar a este numero XXXXX y conseguir que te den la clave de acceso...", answer: "999"}
+    { question: "Reto 4: Tienes que llamar a este numero XXXXX  por videollamada desde un movil que no sea el tuyo y conseguir que te den la clave de acceso...", answer: "1212"}
 ];
 
 let currentChallenge = 0;
@@ -27,10 +27,12 @@ function typeText(element, text, speed = 25) {
 function promptForAccess() {
     const keyword = "2323";
     let attempts = 0;
-    while (attempts < 3) {
-        const userInput = prompt("Introduce la palabra clave para acceder a los retos: La clave de acceso se encuentra en el arbol de navidad. Recuerda todas las respuestas son numericas").toLowerCase();
+    while (attempts < 5) {
+        const userInput = prompt(`Introduce la palabra clave para acceder a los retos: 
+            La clave de acceso se encuentra en el arbol de navidad. Recuerda todas las respuestas son numericas 
+            y tienes 5 intentos para acceder. Si tienes que usar la calculadora o Google hazlo desde otro movil`).toLowerCase();
         if (userInput === keyword) {
-            typeText(outputDiv, "Hola Andrea! Papa Noel te ha traido un regalito pero ha tenido un problema, los elfos han perdido el codigo de accceso al regalo, vas a tener que encontrarlo.\nEscribe la respuesta al primer reto para continuar:\n\n" + challenges[0].question);
+            typeText(outputDiv, "Hola Andrea! Papa Noel te ha traido un regalito pero ha tenido un problema, los elfos han perdido el codigo del candado donde guardan el regalo, vas a tener que encontrarlo para poder abrirlo.\nEscribe la respuesta al primer reto para continuar:\n\n" + challenges[0].question);
             return;
         } else {
             alert("Palabra clave incorrecta. Intenta nuevamente.");
