a616b8c63e98f74f69d8185255f77add22f2560c
Gr44n91 <gr44n91@gmail.com>
2024-12-15 11:25:32 +0100

    mas cambios en redaccion

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const challenges = [
     { question: "Reto 1: Ponme de lado y soy todo. Córtame por la mitad y no soy nada. ¿Qué soy?", answer: "8" },
-    { question: "Reto 2: ¿Cuantos cuchillos hay en el cajón de los cubiertos de la cocina?", answer: "10" },
+    { question: "Reto 2: Los elfos tienen hambre, estaria bien ofrecerle una naranja...", answer: "4525" },
     { question: "Reto 3: ¿Cuantas Bolas tiene el arbol de navidad?", answer: "10" },
-    { question: "Reto 4: Tienes que llamar a este numero XXXXX  por videollamada desde un movil que no sea el tuyo y conseguir que te den la clave de acceso...", answer: "1212"}
+    { question: "Reto 4: Tienes que llamar a este numero XXXXX por videollamada desde un movil que no sea el tuyo y conseguir que te den la clave de acceso...", answer: "1212"}
 ];
 
 let currentChallenge = 0;
@@ -25,11 +25,11 @@ function typeText(element, text, speed = 25) {
 //typeText(outputDiv, "Hola Andrea! Papa Noel te ha traido un regalito pero ha tenido un problema, los elfos han perdido el codigo de accceso al regalo, vas a tener que encontrarlo.\nEscribe la respuesta al primer reto para continuar:\n\n" + challenges[0].question);
 
 function promptForAccess() {
-    const keyword = "2323";
+    const keyword = "19112178";
     let attempts = 0;
     while (attempts < 5) {
         const userInput = prompt(`Introduce la palabra clave para acceder a los retos: 
-            La clave de acceso se encuentra en el arbol de navidad. Recuerda todas las respuestas son numericas 
+            La clave de acceso se encuentra en el arbol de navidad. Recuerda todas las respuestas son numéricas 
             y tienes 5 intentos para acceder. Si tienes que usar la calculadora o Google hazlo desde otro movil`).toLowerCase();
         if (userInput === keyword) {
             typeText(outputDiv, "Hola Andrea! Papa Noel te ha traido un regalito pero ha tenido un problema, los elfos han perdido el codigo del candado donde guardan el regalo, vas a tener que encontrarlo para poder abrirlo.\nEscribe la respuesta al primer reto para continuar:\n\n" + challenges[0].question);
